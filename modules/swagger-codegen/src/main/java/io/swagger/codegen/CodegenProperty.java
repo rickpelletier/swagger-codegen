0eed446bc4ade3b41451f579a5e5e3a6ec9a62ea
Rick Pelletier <richardcpelletier@gmail.com>
2015-07-28 15:41:30 -0400

    Models, collections, and router working.

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/CodegenProperty.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/CodegenProperty.java
@@ -31,7 +31,7 @@ public class CodegenProperty {
     public Boolean exclusiveMaximum;
     public Boolean hasMore = null, required = null, secondaryParam = null;
     public Boolean isPrimitiveType, isContainer, isNotContainer;
-    public boolean isEnum;
+    public boolean isEnum, isDateTime;
     public List<String> _enum;
     public Map<String, Object> allowableValues;
 }
